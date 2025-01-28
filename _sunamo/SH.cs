4d1c1c038b021d0994e7ad3b2f86723e7b68a9bb
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:34 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/SH.cs
+++ _sunamo/SH.cs
@@ -2,10 +2,6 @@ namespace SunamoGetFiles._sunamo;
 
 internal class SH
 {
-    internal static void FirstCharUpper(ref string nazevPP)
-    {
-        nazevPP = FirstCharUpper(nazevPP);
-    }
 
     internal static string FirstCharUpper(string nazevPP)
     {
@@ -18,34 +14,4 @@ internal class SH
         return nazevPP[0].ToString().ToUpper() + sb;
     }
 
-    internal static string JoinNL(List<string> l)
-    {
-        StringBuilder sb = new();
-        foreach (var item in l) sb.AppendLine(item);
-        var r = string.Empty;
-        r = sb.ToString();
-        return r;
-    }
-    internal static List<string> SplitCharMore(string s, params char[] dot)
-    {
-        return s.Split(dot, StringSplitOptions.RemoveEmptyEntries).ToList();
-    }
-    internal static List<string> SplitMore(string s, params string[] dot)
-    {
-        return s.Split(dot, StringSplitOptions.RemoveEmptyEntries).ToList();
-    }
-    internal static List<string> SplitNone(string text, params string[] deli)
-    {
-        return text.Split(deli, StringSplitOptions.None).ToList();
-    }
-    internal static string NullToStringOrDefault(object n)
-    {
-
-        return n == null ? " " + "(null)" : " " + n;
-    }
-    internal static string TrimEnd(string name, string ext)
-    {
-        while (name.EndsWith(ext)) return name.Substring(0, name.Length - ext.Length);
-        return name;
-    }
 }
--- _sunamo/SunamoFileSystem/FS.cs
+++ _sunamo/SunamoFileSystem/FS.cs
@@ -155,10 +155,6 @@ internal class FS
         }
     }
 
-    internal static void CreateUpfoldersPsysicallyUnlessThere(string nad)
-    {
-        CreateFoldersPsysicallyUnlessThere(Path.GetDirectoryName(nad));
-    }
     internal static void CreateFoldersPsysicallyUnlessThere(string nad)
     {
         ThrowEx.IsNullOrEmpty("nad", nad);
